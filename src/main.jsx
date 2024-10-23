318a6e59665b5f8218609673ec694f1f9000ff05
DadMed <mohammed.dad.ens@gmail.com>
2024-10-24 00:59:10 +0100

    all files

--- src/main.jsx
+++ src/main.jsx
@@ -4,42 +4,28 @@ import App from "./App";
 import "./index.css";
 import { action as registerAction } from "./pages/Register/RegisterPage";
 import { action as loginAction } from "./pages/Login/LoginPage";
-import RootLayout from "./layout/RootLayout"
+import RootLayout from "./layout/RootLayout";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
 
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+// Import LandingPage
+import { LoginPage, Question, RegisterPage, NotFound, SingleQuestion, RequireAuth, RequireLogout, Success, Landingpage } from "./pages";
 
-import {
-  LoginPage,
-  Question,
-  RegisterPage,
-  NotFound,
-  SingleQuestion,
-  RequireAuth,
-  RequireLogout,
-  Success,
-} from "./pages";
-
+// Define the router with LandingPage as the default route
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />} errorElement={<NotFound />}>
+      {/* Set LandingPage as the default route */}
+      <Route index element={<Landingpage />} />
+
+      {/* User needs to logout to access these routes */}
       <Route element={<RequireLogout />}>
-        {/* User Need to logout */}
         <Route path="login" element={<LoginPage />} action={loginAction} />
-        <Route
-          path="register"
-          element={<RegisterPage />}
-          action={registerAction}
-        />
+        <Route path="register" element={<RegisterPage />} action={registerAction} />
       </Route>
 
+      {/* User needs to login to access these routes */}
       <Route element={<RequireAuth />}>
-        {/* User need to login */}
-        <Route index element={<App />} />
+        <Route path="quiz" element={<App />} /> {/* You can move App to another route like /quiz */}
         <Route path="question" element={<Question />} />
         <Route path="question/:id" element={<SingleQuestion />} />
         <Route path="finish" element={<Success />} />
@@ -48,6 +34,7 @@ const router = createBrowserRouter(
   )
 );
 
+// Render the app
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
